86f6bfd27004126c1a156dad640997f32ed6790f
Y-Suzaki <geranium04.24.1981@gmail.com>
2017-11-26 22:06:19 +0900

    init

---
+++ web-front-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <p>{{ userid }}</p>
+    <input type="text" v-model="userid">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      userid: 'xxxx'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
